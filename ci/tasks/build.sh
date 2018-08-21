395a1da22536256929d8dec2cb5ce3b9eb96fafd
Matt Segvich <msegvich@pivotal.io>
2018-08-21 14:18:50 -0500

    Pipeline updates

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -7,8 +7,7 @@ ls -l ../
 echo "starting build ..."
 
 dotnet restore
-dotnet publish -c Release 
-dotnet publish -r ubuntu.14.04-x64 -c Release
+dotnet publish -r ubuntu.14.04-x64 -c Release -o ./build-output
 
 # echo "copying files to ../build-output"
 # cp manifest.yml ../build-output
