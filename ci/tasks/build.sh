60a39c9857d8f433abc47aefcdbda85d150317a1
Matt Segvich <msegvich@pivotal.io>
2018-08-21 12:42:54 -0500

    Updating build task

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -9,6 +9,6 @@ echo "starting build ..."
 dotnet restore
 dotnet publish -c Release -o ./publish
 
-echo "copying files to ../build-output"
-cp manifest.yml ../build-output
-cp -R ./publish/* ../build-output
+# echo "copying files to ../build-output"
+# cp manifest.yml ../build-output
+# cp -R ./publish/* ../build-output
