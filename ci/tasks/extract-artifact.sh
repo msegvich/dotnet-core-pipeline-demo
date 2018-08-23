97a532f80144c572828985366f204f965e342a0f
Matt Segvich <msegvich@pivotal.io>
2018-08-22 23:21:45 -0500

    Fix extracting

--- ci/tasks/extract-artifact.sh
+++ ci/tasks/extract-artifact.sh
@@ -7,6 +7,6 @@ cd release-candidate
 echo I am in `pwd`
 ls -l
 
-tar -xvzf ../../artifact-dir/${base_name}-${version}.tar.gz ../expanded-artifact-dir
+tar -xvzf ${base_name}-${version}.tar.gz -C ../expanded-artifact-dir
 echo "In ../expanded-artifact-dir"
 ls ../expanded-artifact-dir
